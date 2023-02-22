e23c3be9a9d7cc3085fc0cbb4e2a8501054c01d2
MarianaGriot <109477310+MarianaGriot@users.noreply.github.com>
2023-02-21 23:49:26 -0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Home = () =>
+    <section className="Home">
+      <h3>Hello Home</h3>
+    </section>
+
+import { Nosotros } from "./routes/Nosotros";
+import { Productos } from "./routes/Productos";
+import { Contacto } from "./routes/Contacto";
+
+
